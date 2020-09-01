c23ed3dbd23bdd47839f97671b2332537f239fcd
Luyolo <luyolom19@gmail.com>
2020-09-01 19:14:46 +0200

    added other required middleware

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+
+const GreetingsFactory = require('./greetings');
+const greetingsFactory = GreetingsFactory();
+
+// declaring my port number
+let PORT = process.env.PORT || 1997;
+app.listen(PORT, function(){
+	console.log('App started on port:', PORT);
+})
 
 // importing handlebars
 const exhbs = require('express-handlebars');
@@ -7,19 +17,18 @@ const exhbs = require('express-handlebars');
 app.engine('handlebars', exhbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// make public folder visible -- (a middleware)
+app.use(express.static('public'));
 
+// parse appliction
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application / json
+app.use(bodyParser.json());
+
+// my routes
 // rendering the index greetings handlebars template
 app.get('/', function(req, res){
 	res.render('index');
 })
 
-
-app.get("/", function(req, res){
-	res.send('Greetings Webapp');
-})
-
-let PORT = process.env.PORT || 1997;
-
-app.listen(PORT, function(){
-	console.log('App started on port:', PORT);
-})
